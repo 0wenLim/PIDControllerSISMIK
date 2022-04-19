1f8e337b2aa78dafe3f10799491fc42feca3c13c
Owen Limvin <73385374+0wenLim@users.noreply.github.com>
2022-04-19 07:57:39 +0700

    Add files via upload

--- controller-plant/controller-plant.ino
+++ controller-plant/controller-plant.ino
@@ -1,4 +1,4 @@
-int input = 1;
+int input = 0;
 int i = 0;
 
 String bacaan;
@@ -33,8 +33,15 @@ void loop() {
   
   currentErr = (float)input - prevOutput;
   currentCtrl = (1/a)*((a*Kp+Ki+pow(a,2)*Kd)*currentErr + 2*(Ki-pow(a,2)*Kd)*prevErr + (pow(a,2)*Kd-a*Kp+Ki)*prev2Err + a*prev2Ctrl) ;
+  
+  //kendali posisi
   currentOutput = (2*pow(a,2)-20)/(pow(a,2)+7*a+10)*prevOutput - (pow(a,2)-7*a+10)/(pow(a,2)+7*a+10)*prev2Output + 0.75/(pow(a,2)+7*a+10)*currentCtrl + 1.5/(pow(a,2)+7*a+10)*prevCtrl + 0.75/(pow(a,2)+7*a+10)*prev2Ctrl;
 
+  //kendali kecepatan
+  //currentOutput = (1/(a+5))*(0.9*currentCtrl - 0.9*prevCtrl + (5-1)*prevOutput);
+
+  Serial.print(input);
+  Serial.print(" ");
   Serial.println(currentOutput);
 
   delay(T);
