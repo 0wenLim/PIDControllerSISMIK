7bc9fff3df0877321b2c4865e36e282c3618c90e
Owen Limvin <73385374+0wenLim@users.noreply.github.com>
2022-05-15 23:19:09 +0700

    tambah comment

--- controller/controller.ino
+++ controller/controller.ino
@@ -1,9 +1,12 @@
+//kode untuk bagian controller + input target dan variabel kendali
+//include library
 #include <SoftwareSerial.h>
 #include <math.h>
 
-float input = 0;
-String bacaan;
+//deklarasi variabel
+float input = 0; //target (bisa diinput juga saat run
 
+//variabel PID
 float Kp = 10;
 float Kd = 0.1;
 float Ki = 5;
@@ -13,46 +16,49 @@ float a = 2/T;
 float currentErr = 0;
 float prevErr = 0;
 float prev2Err = 0;
-
 float currentCtrl = 0;
 float prevCtrl = 0;
 float prev2Ctrl = 0;
-
 float currentOutput = 0;
 float prevOutput = 0;
 float prev2Output = 0;
 
+//inisialisasi Tx dan Rx yang baru
 SoftwareSerial mySerial = SoftwareSerial(5, 6);
 
+//setup baud rate
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 }
 
 void loop() {
+  //terima target dari serial
   if (Serial.available() > 0) {
     input = Serial.parseFloat(SKIP_ALL, '\n');
   }
-  
+  //baca output plant
   if (mySerial.available() > 0) {
     currentOutput = mySerial.parseFloat(SKIP_ALL, '\n');
   }
   
+  //hitung error
   currentErr = input - prevOutput;
+
+  //rumus PID
   currentCtrl = (1.0/a)*((a*Kp+Ki+powf(a,2)*Kd)*currentErr + 2.0*(Ki-powf(a,2)*Kd)*prevErr + (powf(a,2)*Kd-a*Kp+Ki)*prev2Err + a*prev2Ctrl) ;
-  
+
+  //output input ke serial monitor, output cntroller ke plant
   Serial.println(input);
   mySerial.println(currentCtrl);
  
   delay(T);
-  
+
+  //simpan iterasi sebelumnya
   prev2Err = prevErr;
   prevErr = currentErr;
-
   prev2Ctrl = prevCtrl;
   prevCtrl = currentCtrl;
-
   prev2Output = prevOutput;
   prevOutput = currentOutput;
-
 }
--- plant/plant.ino
+++ plant/plant.ino
@@ -1,7 +1,9 @@
+//kode untuk bagian controller + input target dan variabel kendali
+//include library
 #include <SoftwareSerial.h>
 #include <math.h>
 
-float input = 0;
+//deklarasi variabel
 
 float T = 0.02;
 float a = 2/T;
@@ -9,46 +11,43 @@ float a = 2/T;
 float currentErr = 0;
 float prevErr = 0;
 float prev2Err = 0;
-
 float currentCtrl = 0;
 float prevCtrl = 0;
 float prev2Ctrl = 0;
-
 float currentOutput = 0;
 float prevOutput = 0;
 float prev2Output = 0;
 
+//inisialisasi Tx dan Rx yang baru
 SoftwareSerial mySerial = SoftwareSerial(5, 6);
 
+//setup baud rate
 void setup() {
   Serial.begin(9600);
   mySerial.begin(9600);
 }
 
 void loop() {
-
+  //baca output controller
   if (mySerial.available() > 0) {
     currentCtrl = mySerial.parseFloat(SKIP_ALL, '\n');
   }
 
-  //kendali posisi
+  //rumus plant posisi
   //currentOutput = (2*powf(a,2)-20)/(powf(a,2)+7*a+10)*prevOutput - (powf(a,2)-7*a+10)/(powf(a,2)+7*a+10)*prev2Output + 0.75/(powf(a,2)+7*a+10)*currentCtrl + 1.5/(powf(a,2)+7*a+10)*prevCtrl + 0.75/(powf(a,2)+7*a+10)*prev2Ctrl;
 
-  //kendali kecepatan
+  //rumus plant kecepatan
   currentOutput = (1.0/(5.0+a))*(0.9*currentCtrl + 0.9*prevCtrl - (5.0-a)*prevOutput);
 
+  //output sistem dikirim ke serial monitor dan controller
   Serial.println(currentOutput);
   mySerial.println(currentOutput);
 
   delay(T);
   
-  prev2Err = prevErr;
-  prevErr = currentErr;
-
+  //simpan iterasi sebelumnya
   prev2Ctrl = prevCtrl;
   prevCtrl = currentCtrl;
-
   prev2Output = prevOutput;
   prevOutput = currentOutput;
-
 }
